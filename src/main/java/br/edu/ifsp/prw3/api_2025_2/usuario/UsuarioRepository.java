11a240baf06a3212389a5b02e70843f749ae9a3d
Carlos Jose Pereira <carlaopereirasanca@gmail.com>
2025-11-03 15:12:11 -0300

    apos security parte 1

---
+++ src/main/java/br/edu/ifsp/prw3/api_2025_2/usuario/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package br.edu.ifsp.prw3.api_2025_2.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByLogin(String login);
+
+}
+
